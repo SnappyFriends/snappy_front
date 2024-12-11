02fbfc12659fd1290c7898d58810f36ded44b44f
Dan <carlosdaniellazo06@gmail.com>
2024-12-11 02:41:13 -0300

    fix-loadingbar

--- src/app/loadingbar/page.tsx
+++ src/app/loadingbar/page.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+'use client';
+
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 export default function LoadingBar() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/newchat");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      
-      <p className="text-lg font-semibold mb-4">Buscando a tu persona ideal...</p>
+
+      <p className="text-lg font-semibold mb-4">
+        Buscando a tus nuevos amigos...
+      </p>
 
       <div className="w-64 bg-gray-200 rounded-full h-2.5 overflow-hidden">
         <div
