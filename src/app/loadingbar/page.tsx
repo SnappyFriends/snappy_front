dc70d7f34643b3f1dcee177e4f09d21c5224cff5
Dan <carlosdaniellazo06@gmail.com>
2024-12-11 03:05:32 -0300

    agregue barra de carga animada para que muestre en tiempo real su progreso

--- src/app/loadingbar/page.tsx
+++ src/app/loadingbar/page.tsx
@@ -1,30 +1,44 @@
-'use client';
+"use client";
 
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function LoadingBar() {
+  const [progress, setProgress] = useState(0);
   const router = useRouter();
 
   useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev < 100) {
+          return prev + 1;
+        } else {
+          clearInterval(interval);
+          return 100;
+        }
+      });
+    }, 50);
+
     const timer = setTimeout(() => {
       router.push("/newchat");
     }, 5000);
 
-    return () => clearTimeout(timer);
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timer);
+    };
   }, [router]);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-
       <p className="text-lg font-semibold mb-4">
-        Buscando a tus nuevos amigos...
+        Buscando...
       </p>
 
       <div className="w-64 bg-gray-200 rounded-full h-2.5 overflow-hidden">
         <div
           className="bg-blue-500 h-2.5 animate-pulse"
-          style={{ width: "70%" }}
+          style={{ width: `${progress}%` }}
         ></div>
       </div>
     </div>
