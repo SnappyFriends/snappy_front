0b13dd05619b62f83334af5b477546104ee6c65c
Dan <carlosdaniellazo06@gmail.com>
2024-12-12 06:03:48 -0300

    mensajes privados

--- src/app/mensajesprivados/page.tsx
+++ src/app/mensajesprivados/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function MensajesPrivados() {
   return (
     <>
-      <nav className="h-16 flex justify-center items-center ">
+      <nav className="h-16 flex justify-center items-center">
         <form className="w-full px-4 flex justify-center items-center">
           <input
             type="text"
@@ -25,6 +25,7 @@ export default function MensajesPrivados() {
           </button>
         </form>
       </nav>
+
       <main className="">
         <div>
           <h2 className="text-center my-2">Mensajes</h2>
@@ -52,15 +53,15 @@ export default function MensajesPrivados() {
         </div>
       </main>
 
-      <nav>
-        <div className="bg-red-700">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="white">
-              <path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
-            </svg>
-          </div>
+      <div className="fixed bottom-0 w-full h-20 bg-white">
+        <div className="flex justify-between items-center h-full px-4">
+          <Image src="/home.png" width={50} height={50} alt="home" className="self-center" />
+          <Image src="/chat.png" width={50} height={50} alt="chats" className="self-center" />
+          <Image src="/logochatsnuevos.png" width={50} height={50} alt="chats nuevos" className="self-center" />
+          <Image src="/bell.png" width={50} height={50} alt="notificaciones" className="self-center" />
+          <Image src="/user.png" width={50} height={50} alt="perfil" className="self-center" />
         </div>
-      </nav>
+      </div>
     </>
   );
 }
