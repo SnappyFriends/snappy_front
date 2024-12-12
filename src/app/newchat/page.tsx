c800755506c755a7c75b1c2ad4252f012a492afd
veronica <veronica.lee1092@gmail.com>
2024-12-12 18:13:03 -0300

    vista newchat con navbar integrada

--- src/app/newchat/page.tsx
+++ src/app/newchat/page.tsx
@@ -2,9 +2,12 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 const ChatView = () => {
   return (
+    <div>
+    <Navbar/>
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white rounded-lg shadow-md w-1/2 max-w-lg min-h-[50vh]">
         <div className="flex items-center justify-between px-4 py-3 border-b">
@@ -71,6 +74,7 @@ const ChatView = () => {
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
