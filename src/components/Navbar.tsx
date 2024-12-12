82116c367a8e8713c1c830ac046f2ff9e197134b
veronica <veronica.lee1092@gmail.com>
2024-12-12 18:24:59 -0300

    agregue link al social feed desde el logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-4">
-            <Link href="/favicon">
+            <Link href="/socialfeed">
               <Image
                 src="/favicon.ico"
                 alt="Left Icon"
