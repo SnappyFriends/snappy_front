a90b2d5c9cd9778faeb8437d82c12fdff63e0af7
veronica <veronica.lee1092@gmail.com>
2024-12-15 17:30:48 -0300

    agrego componente de conectaods a social feed y a las notificaciones

---
+++ src/components/Conectados.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { getUsers } from "@/helpers/users"; 
+import Link from "next/link"; 
+
+interface User {
+  id: string;
+  username: string;
+  profile_image: string;
+}
+
+const Conectados: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const fetchedUsers: User[] = await getUsers();
+        setUsers(fetchedUsers);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  const firstSixUsers = users.slice(0, 6);
+
+  return (
+    <div className="space-y-4 p-4 rounded-lg ml-24">
+      {firstSixUsers.length === 0 ? (
+        <p className="text-gray-500 text-sm">No hay usuarios disponibles en este momento.</p>
+      ) : (
+        firstSixUsers.map(user => (
+          <Link key={user.id} href={`/mensajesprivados`}>
+            <div className="flex items-center space-x-4 p-2 rounded-lg hover:bg-gray-100 transition cursor-pointer ml-auto">
+              <div className="relative w-12 h-12">
+                <Image
+                  src="/agregarfoto.png" 
+                  alt={user.username}
+                  layout="fill"
+                  className="rounded-full object-cover"
+                />
+              </div>
+              <h3 className="text-sm font-semibold">{user.username}</h3>
+              <span className="w-3 h-3 bg-green-500 rounded-full self-center"></span> 
+            </div>
+          </Link>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Conectados;
+
+
