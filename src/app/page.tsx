91ef823649737077be8aebaefad0b9989651ba0d
Dan <carlosdaniellazo06@gmail.com>
2024-12-15 15:53:54 -0300

    prueba quitada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import Login from "@/components/Login";
-import { useContext } from "react";
-import { UserContext } from "@/context/UserContext";
 
 export default function Home() {
-  const { prueba } = useContext(UserContext);
 
   return (
     <>
