c3ee43c849edced3601f7ca034b0f87e8722d35a
veronica <veronica.lee1092@gmail.com>
2024-12-15 12:36:32 -0300

    habia comentado el middleware para poder ver las vistas y ahora lo descomente para que uqede como el que hizo Dan

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,9 @@ export function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
     const isLoggedIn = req.cookies.get('auth_token');
 
-    // if (!isLoggedIn && pathname !== '/' && pathname !== '/register') {
-    //     return NextResponse.redirect(new URL('/', req.url));
-    // }
+    if (!isLoggedIn && pathname !== '/' && pathname !== '/register') {
+        return NextResponse.redirect(new URL('/', req.url));
+    }
 
     if (isLoggedIn && (pathname === '/' || pathname === '/register')) {
         return NextResponse.redirect(new URL('/socialfeed', req.url));
