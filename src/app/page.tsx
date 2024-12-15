e8f3ee436f3aca51eb4e747a6d47d48e4e4ce114
Dan <carlosdaniellazo06@gmail.com>
2024-12-15 01:07:39 -0300

    home actualizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-'use client';
+"use client";
 
-import HeaderLoginRegister from "@/components/HeaderLoginRegister";
 import Login from "@/components/Login";
-import FooterLoginRegister from "../components/FooterLoginRegister";
 import { useContext } from "react";
 import { UserContext } from "@/context/UserContext";
 
@@ -10,11 +8,8 @@ export default function Home() {
   const { prueba } = useContext(UserContext);
 
   return (
-    <div className="flex flex-col items-center justify-center gap-16 min-h-screen">
-      <h1>{prueba}</h1>
-      <HeaderLoginRegister />
+    <>
       <Login />
-      <FooterLoginRegister />
-    </div>
+    </>
   );
 }
