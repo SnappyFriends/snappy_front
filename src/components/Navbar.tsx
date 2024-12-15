878558d9b5c1e4db9d1bba0e9b53754d7f7e52f9
Dan <carlosdaniellazo06@gmail.com>
2024-12-15 17:28:38 -0300

    cookies implementadas

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,87 +1,104 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import SearchBar from "./Searchbar";
+import Cookies from "js-cookie";
+import { UserContext } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { setToken } = useContext(UserContext);
+  const router = useRouter();
+
+  const handleLogout = () => {
+    Cookies.remove("auth_token");
+
+    setToken(null);
+
+    router.push("/");
+  };
 
   const icons = [
     { href: "/socialfeed", src: "/home1.png", alt: "Home Icon" },
     { href: "/mensajesprivados", src: "/mensajes.png", alt: "Messages Icon" },
     { href: "/newchat", src: "/logochatsnuevos.png", alt: "Arrow Icon" },
-    { href: "/notificaciones", src: "/notificaciones.png", alt: "Notifications Icon" },
+    {
+      href: "/notificaciones",
+      src: "/notificaciones.png",
+      alt: "Notifications Icon",
+    },
     { href: "/miperfil", src: "/usuario.png", alt: "Profile Icon" },
   ];
 
   return (
     <nav className="bg-white text-gray-800 shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between items-center h-16">
         <Link href="/socialfeed">
           <div className="flex items-center space-x-4 flex-shrink-0">
-              <Image
-                src="/favicon.ico"
-                alt="Left Icon"
-                width={50}
-                height={50}
-              />
-            
+            <Image src="/favicon.ico" alt="Left Icon" width={50} height={50} />
             <span className="text-2xl font-bold text-gray-800">SNAPPY FRIENDS</span>
-            
           </div>
-          </Link>
+        </Link>
 
-          <div className="flex-grow ml-48">
-            <SearchBar />
-          </div>
-
-          <div className="hidden md:flex space-x-6">
-            {icons.map(({ href, src, alt }) => (
-              <Link href={href} key={href}>
-                <Image src={src} alt={alt} width={32} height={32} />
-              </Link>
-            ))}
-          </div>
-
-          <div className="md:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="text-gray-800 focus:outline-none"
-              aria-label="Abrir menú"
-              title="Abrir menú"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            </button>
-          </div>
+        <div className="flex-grow ml-48">
+          <SearchBar />
         </div>
 
-        {isOpen && (
-          <div className="md:hidden mt-4 space-y-4">
-            {icons.map(({ href, src, alt }) => (
-              <Link href={href} key={href} className="block">
-                <Image src={src} alt={alt} width={32} height={32} />
-              </Link>
-            ))}
-          </div>
-        )}
+        <div className="hidden md:flex items-center space-x-6">
+          {icons.map(({ href, src, alt }) => (
+            <Link href={href} key={href}>
+              <Image src={src} alt={alt} width={32} height={32} />
+            </Link>
+          ))}
+          {/* Botón de cerrar sesión colocado aquí */}
+          <button
+            onClick={handleLogout}
+            className="text-red-600 hover:text-red-800"
+          >
+            Cerrar sesión
+          </button>
+        </div>
+
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-gray-800 focus:outline-none"
+            aria-label="Abrir menú"
+            title="Abrir menú"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-    </nav>
+
+      {isOpen && (
+        <div className="md:hidden mt-4 space-y-4">
+          {icons.map(({ href, src, alt }) => (
+            <Link href={href} key={href} className="block">
+              <Image src={src} alt={alt} width={32} height={32} />
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  </nav>
   );
 };
 
