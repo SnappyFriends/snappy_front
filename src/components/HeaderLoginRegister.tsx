f3385d4799bccdf698bbad1f5899672048402b77
Dan <carlosdaniellazo06@gmail.com>
2024-12-16 03:00:18 -0300

    login

--- src/components/HeaderLoginRegister.tsx
+++ src/components/HeaderLoginRegister.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 export default function HeaderLoginRegister() {
   return (
-    <header className="w-full p-4 bg-gray-100">
-      <div className="flex flex-col items-center md:flex-row md:justify-between">
-      <div className="mb-4 md:mb-0">
-        <Image
-        src="/favicon.ico"
-        alt="Snappy logo"
-        width={150}
-        height={150}
-        />
-      </div>
-      <div>
-        <h1 className="text-3xl font-bold text-center md:text-left">SNAPPY FRIENDS</h1>
-      </div>
+    <header className="w-full">
+      <div className="flex flex-col items-center">
+        <div>
+          <Image
+            src="/favicon.ico"
+            alt="Snappy logo"
+            width={150}
+            height={150}
+          />
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold">SNAPPY FRIENDS</h1>
+        </div>
       </div>
     </header>
   );
