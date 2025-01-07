8d79971e16c19a9165474ec84bedf5ea87a31d42
Dan <carlosdaniellazo06@gmail.com>
2025-01-07 00:45:23 -0300

    foto de perfil y intereses obligatorios

--- src/app/fotodeperfil/page.tsx
+++ src/app/fotodeperfil/page.tsx
@@ -1,55 +1,10 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
+import FotoDePerfilRegistro from "@/components/FotoDePerfilRegistro";
 
 export default function ProfilePicture() {
   return (
-    <div>
-      <div className="flex flex-col items-center mt-10 mb-5">
-        <div>
-          <Image
-            src="/favicon.ico"
-            alt="Snappy logo"
-            width={300}
-            height={301}
-          />
-        </div>
-      </div>
-      <main className="flex justify-center w-full">
-        <div className="flex flex-col items-center w-96 gap-5">
-          <h2 className="font-bold text-xl mt-10 mb-6">Elige tu foto de perfil</h2>
-
-          <div className="relative flex justify-center mb-10"> 
-            <Link href="/profilepicture">
-              <Image
-                src="/agregarfoto.png"
-                alt="Icon Link"
-                width={120}
-                height={120}
-                className="cursor-pointer"
-              />
-            </Link>
-
-            <Link href="/profilepicture">
-              <Image
-                src="/lapiz.png"
-                alt="Small Icon"
-                width={20}
-                height={20}
-                className="absolute bottom-0 right-0 cursor-pointer"
-              />
-            </Link>
-          </div>
-
-          
-          <button
-            className="w-80 h-12 bg-black border border-none rounded-md text-xl text-white"
-            type="submit"
-          >
-            Siguiente
-          </button>
-        </div>
-      </main>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-center font-bold text-2xl mb-6">Elige tu foto de perfil</h1>
+      <FotoDePerfilRegistro />;
     </div>
   );
 }
---
+++ src/app/intereses/page.tsx
@@ -0,0 +1,10 @@
+import InteresesRegistro from "@/components/InteresesRegistro";
+import React from "react";
+
+export default function Intereses() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <InteresesRegistro />
+    </div>
+  );
+}
