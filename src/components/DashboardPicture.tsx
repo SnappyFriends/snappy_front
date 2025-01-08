282d6feca9f285e61409452563ca122b623310a0
Artifexted <artifexted@gmail.com>
2025-01-08 00:13:33 -0300

    Navbar corregido, achicadas algunas partes del dashboard

--- src/components/DashboardPicture.tsx
+++ src/components/DashboardPicture.tsx
@@ -6,29 +6,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 const DashboardPicture = () => {
-  const { userData } = useContext(UserContext);
+	const { userData } = useContext(UserContext);
 
-  return (
-    <nav className="bg-gray-50 flex justify-between items-center h-40 px-4 w-[80%] ml-[20%] fixed top-0 left-0">
-      <h1 className=" text-black font-extrabold text-2xl justify-center "></h1>      
-        <div className="flex flex-row items-center mt-2 mr-10 cursor-pointer">
-            <Link href={`/perfil/${userData?.username}`}>
-          <Image
-            src={userData?.profile_image || "/default-profile.png"}
-            alt={userData?.username || "Profile Picture"}
-            width={80}
-            height={80}
-            className="rounded-full border-2 mr-5"
-          />
-           </Link>
-          <p className="text-black text-center font-semibold text-xl">
-            {userData?.username}
-          </p>
-         
-         
-        </div>
-    </nav>
-  );
+	return (
+		<nav className="bg-gray-50 flex justify-between items-center h-40 px-4 w-[80%] ml-[20%] fixed top-0 left-0">
+			<h1 className=" text-black font-extrabold text-2xl justify-center "></h1>
+			<div className="flex flex-row items-center mt-2 mr-10 cursor-pointer">
+        <p className="text-black text-center font-semibold text-xl">
+        {userData?.username}
+      </p>
+				<Link href={`/perfil/${userData?.username}`}>
+					<Image
+						src={userData?.profile_image || "/default-profile.png"}
+						alt={userData?.username || "Profile Picture"}
+						width={60}
+						height={60}
+						className="rounded-full border-2 mr-5"
+					/>
+				</Link>
+			</div>
+		</nav>
+	);
 };
 
 export default DashboardPicture;
--- src/components/DashboardSideBar.tsx
+++ src/components/DashboardSideBar.tsx
@@ -5,73 +5,103 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 const DashboardSidebar = () => {
-  const sideBarList = ["Usuarios", "Publicaciones", "Comentarios", "Reportes", "Estadisticas"];
-  
-  const [isOpen, setIsOpen] = useState(false);
+	const sideBarList = [
+		"Usuarios",
+		"Publicaciones",
+		"Comentarios",
+		"Reportes",
+		"Estadisticas",
+	];
 
-  const toggleMenu = () => {
-    setIsOpen((prevState) => !prevState);
-  };
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <>
-      <div className="bg-black h-full w-1/5 fixed z-50 lg:block hidden">
-        <div className="m-auto mt-4 sm:flex items-center justify-center sm:justify-start sm:col-span-1">
-          <Image src="/favicon.ico" alt="Snappy" width={150} height={150} />
-          <h1 className="font-extrabold text-3xl ml-2 text-white">SNAPPY FRIENDS</h1>
-        </div>
-        <div className="mt-20 h-full">
-          <ul>
-            <Link href="/dashboard">
-              <li className="text-white font-bold p-2 text-center text-2xl mb-7">Dashboard</li>
-            </Link>
-            {sideBarList.map((item, index) => (
-              <li key={index} className="text-white font-bold p-2 text-center text-2xl mb-7">
-                <Link href={`dashboard/${item.toLowerCase()}`} className="hover:underline">
-                  {item}
-                </Link>
-              </li>
-            ))}
-            <Link href="/socialfeed">
-              <li className="text-white font-bold p-2 text-center text-2xl mb-7">Social Feed</li>
-            </Link>
-          </ul>
-        </div>
-      </div>
+	const toggleMenu = () => {
+		setIsOpen((prevState) => !prevState);
+	};
 
-      <div className="lg:hidden fixed z-50 w-full bg-black">
-        <div className="flex items-center justify-between p-4">
-          <div className="flex items-center">
-            <Image src="/favicon.ico" alt="Snappy" width={40} height={40} />
-            <h1 className="font-extrabold text-xl ml-2 text-white">SNAPPY FRIENDS</h1>
-          </div>
-          <button onClick={toggleMenu} className="text-white text-3xl">
-            {isOpen ? "✖" : "☰"}
-          </button>
-        </div>
+	return (
+		<>
+			<div className="bg-black h-full w-1/6 fixed z-50 lg:block hidden">
+				<div className="m-auto mt-4 sm:flex items-center justify-center sm:justify-start sm:col-span-1">
+					<Image src="/favicon.ico" alt="Snappy" width={90} height={90} />
+					<h1 className="font-extrabold text-2xl ml-2 text-white">
+						SNAPPY FRIENDS
+					</h1>
+				</div>
+				<div className="mt-20 h-full">
+					<ul>
+						<Link href="/dashboard">
+							<li className="text-white font-bold p-2 text-center text-1xl mb-7">
+								Dashboard
+							</li>
+						</Link>
+						{sideBarList.map((item, index) => (
+							<li
+								key={index}
+								className="text-white font-bold p-2 text-center text-1xl mb-7"
+							>
+								<Link
+									href={`../dashboard/${item.toLowerCase()}`}
+									className="hover:underline"
+								>
+									{item}
+								</Link>
+							</li>
+						))}
+						<Link href="/socialfeed">
+							<li className="text-white font-bold p-2 text-center text-1xl mb-7">
+								Social Feed
+							</li>
+						</Link>
+					</ul>
+				</div>
+			</div>
 
-        {isOpen && (
-          <div className="bg-black text-white p-4 h-[calc(100vh-80px)] overflow-y-auto">
-            <ul>
-              <Link href="/dashboard">
-                <li className="text-white font-bold p-2 text-center text-2xl mb-7">Dashboard</li>
-              </Link>
-              {sideBarList.map((item, index) => (
-                <li key={index} className="text-white font-bold p-2 text-center text-2xl mb-7">
-                  <Link href={`dashboard/${item.toLowerCase()}`} className="hover:underline">
-                    {item}
-                  </Link>
-                </li>
-              ))}
-              <Link href="/socialfeed">
-                <li className="text-white font-bold p-2 text-center text-2xl mb-7">Social Feed</li>
-              </Link>
-            </ul>
-          </div>
-        )}
-      </div>
-    </>
-  );
+			<div className="lg:hidden fixed z-50 w-full bg-black">
+				<div className="flex items-center justify-between p-4">
+					<div className="flex items-center">
+						<Image src="/favicon.ico" alt="Snappy" width={40} height={40} />
+						<h1 className="font-extrabold text-xl ml-2 text-white">
+							SNAPPY FRIENDS
+						</h1>
+					</div>
+					<button onClick={toggleMenu} className="text-white text-3xl">
+						{isOpen ? "✖" : "☰"}
+					</button>
+				</div>
+
+				{isOpen && (
+					<div className="bg-black text-white p-4 h-[calc(100vh-80px)] overflow-y-auto">
+						<ul>
+							<Link href="/dashboard">
+								<li className="text-white font-bold p-2 text-center text-2xl mb-7">
+									Dashboard
+								</li>
+							</Link>
+							{sideBarList.map((item, index) => (
+								<li
+									key={index}
+									className="text-white font-bold p-2 text-center text-2xl mb-7"
+								>
+									<Link
+										href={`dashboard/${item.toLowerCase()}`}
+										className="hover:underline"
+									>
+										{item}
+									</Link>
+								</li>
+							))}
+							<Link href="/socialfeed">
+								<li className="text-white font-bold p-2 text-center text-2xl mb-7">
+									Social Feed
+								</li>
+							</Link>
+						</ul>
+					</div>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default DashboardSidebar;
